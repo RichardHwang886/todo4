72aa4ff2f930ac3b31b87b12796ced5bff32d139
Richard Hwang <richard@Richard-Mac-mini.local>
2016-05-10 17:54:11 +0800

    init

---
+++ server/main.ts
@@ -0,0 +1,11 @@
+import { Meteor } from 'meteor/meteor';
+//import {Parties} from '../collections/parties.ts';
+//import {loadParties} from './load-parties.ts';
+//import './parties.ts';
+
+//Meteor.startup(loadParties);
+
+Meteor.startup(() => {
+    // code to run on server at startup
+});
+
